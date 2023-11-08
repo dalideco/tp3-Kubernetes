5ba278c404ab599953aeb98e74be1b83f8a5018a
dalideco <dalissj1@gmail.com>
2023-11-08 11:07:43 +0100

    node app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('This is a test')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
